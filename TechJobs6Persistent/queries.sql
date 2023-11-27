9df670301e3999da17f9c67692c94c7974bc8ad2
Colleen Blake <colleenemilyblake@gmail.com>
2023-11-26 22:41:33 -0600

    Finished, working, but not adding jobs on APP

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -3,10 +3,16 @@
 
 
 --Part 1: List the columns and their data types in the Jobs table.
+
 --int Id(INT), string Name(TEXT), int EmployeeId(INT)
 
+
 --Part 2: Write a query to list the names of the employers in St. Louis City.
---SELECT * FROM techjobs.employers WHERE Location= "Saint Louis";
+
+--SELECT Name FROM techjobs.employers WHERE Location= "Saint Louis";
+
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
+
+--SELECT SkillName FROM skills INNER JOIN jobskills ON skills.Id = SkillsId WHERE jobskills.JobsId IS NOT NULL ORDER BY SkillName ASC;
